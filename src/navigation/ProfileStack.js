9e83ce40f574eb4f9c593f6c3d20bae11f23601d
sultanmhmddd <sultanmhmd0112@gmail.com>
2026-06-14 18:52:29 +0700

    fix: set profilestack pake native-stack sesuai package json

--- src/navigation/ProfileStack.js
+++ src/navigation/ProfileStack.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import ProfileScreen from '../screens/ProfileScreen'
 
-const Stack = createStackNavigator()
+const Stack = createNativeStackNavigator()
 
 export default function ProfileStack() {
   return (
